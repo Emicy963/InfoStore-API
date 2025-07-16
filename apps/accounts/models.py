375853bc0e9aa07872c9fac8fdd7c9d9c4488c55
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-16 04:54:12 +0100

    feat: creating the user ententy

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'admin', _('Admin')
+        CLIENT = 'client', _('Client')
+
+    email = models.EmailField(_('email address'), unique=True)
+    full_name = models.CharField(_('full name'), max_length=150)
+    phone = models.CharField(_('phone'), max_length=20, blank=True)
+    role = models.CharField(
+        _('role'),
+        max_length=10,
+        choices=Role.choices,
+        default=Role.CLIENT,
+    )
+    created_at = models.DateTimeField(_('cretead at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'full_name']
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.email
