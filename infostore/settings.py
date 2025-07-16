7305c766290eb30436beb5c9ca126051aa39cdbb
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-16 05:26:39 +0100

    feat: refatoring the app accounts and create a Custom User and apply the migrations

--- infostore/settings.py
+++ infostore/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_spectacular',
 
+    # Local apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'accounts.User'
 
 # Django REST Framework
 REST_FRAMEWORK = {
