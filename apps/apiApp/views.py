f34ad8c4e068721322e6aae3b9f8986fb5a6affb
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-27 14:52:55 +0100

    feat: create a api view for category

--- apps/apiApp/views.py
+++ apps/apiApp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProducListSerializer, ProducDetailSerializer
+from .models import Product, Category
+from .serializers import ProducListSerializer, ProducDetailSerializer, CategorySerialiizer
 
 @api_view(['GET'])
 def product_list(requesst):
@@ -14,4 +14,10 @@ def product_list(requesst):
 def product_detail(request, slug):
     products = Product.objects.get(slug=slug)
     serializer = ProducDetailSerializer(products)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def category_list(request):
+    categories = Category.objects.all()
+    serializer = CategorySerialiizer(categories, many=True)
     return Response(serializer.data)
