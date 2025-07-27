36dbfdf56d05421a39607bea6af32059986c9ad8
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-27 14:21:03 +0100

    feat: create a serializer for cetegory

--- apps/apiApp/models.py
+++ apps/apiApp/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     slug = models.SlugField()
     image = models.ImageField(upload_to='product_img', blank=True, null=True)
-    category  = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    category  = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='products', blank=True, null=True)
 
     def __str__(self):
         return self.name
--- apps/apiApp/serializers.py
+++ apps/apiApp/serializers.py
@@ -5,3 +5,9 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'slug', 'description', 'image', 'price']
+
+class CategorySerialiizer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'image' 'products']
