53dc51393733634b2efba1e59e3bcdf3f50129c9
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-28 23:47:21 +0100

    feat: register the product for admin management

--- apps/apiApp/admin.py
+++ apps/apiApp/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Product
 
 class CustomAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name','last_name')
 
 admin.site.register(CustomUser, CustomAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'featured')
+
+admin.site.register(Product, ProductAdmin)
