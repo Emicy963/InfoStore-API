a1c78b844ea9163f4fec9717fbc19ebdb1014187
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-29 23:28:11 +0100

    feat: working in update cart item quantity

--- apps/apiApp/views.py
+++ apps/apiApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product, Category, Cart, CartItem
-from .serializers import ProducListSerializer, ProducDetailSerializer, CategoryListSerialiizer, CategoryDetailSerialiizer, CartSerializer
+from .serializers import ProducListSerializer, ProducDetailSerializer, CategoryListSerialiizer, CategoryDetailSerialiizer, CartSerializer, CartItemSerializer
 
 @api_view(['GET'])
 def product_list(requesst):
@@ -42,3 +42,15 @@ def add_to_cart(request):
 
     serializer = CartSerializer(cart)
     return Response(serializer.data)
+
+@api_view(['PUT'])
+def update_cartitem_quantity(request):
+    cartitem_id = request.data.get('item_id')
+    quantity = request.data.get('quantity')
+
+    cartitem = CartItem.objects.get(id=cartitem_id)
+    cartitem.quantity = quantity
+    cartitem.save()
+
+    serializer = CartItemSerializer(cartitem)
+    return Response({'data': serializer.data, 'message': 'CartItem updated sucessfully!'})
