7333e80c813cc742b1b21bac381d29c6b72439ed
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-30 00:46:17 +0100

    feat: create a signal for update the rating on delete

--- apps/apiApp/signals.py
+++ apps/apiApp/signals.py
@@ -15,3 +15,16 @@ def update_product_rating_on_save(sender, instance, **kwargs):
     product_rating.average_rating = reviews_average
     product_rating.total_reviews = total_reviews
     product_rating.save()
+
+@receiver(post_delete, sender=Review)
+def update_product_rating_on_delete(sender, instance, **kwargs):
+    product = instance.product
+    reviews = product.reviews.all()
+    total_reviews = reviews.count()
+
+    reviews_average = reviews.aggregate(Avg('rating'))['rating__avg'] or 0.0
+
+    product_rating, created = ProductRating.objects.get_or_create(product=product)
+    product_rating.average_rating = reviews_average
+    product_rating.total_reviews = total_reviews
+    product_rating.save()
