1c628bef8042c2fd2ca09f8260ef88fb6824eaf6
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-29 01:23:17 +0100

    feat: add the cart in admin panel

--- apps/apiApp/admin.py
+++ apps/apiApp/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Product, Category
+from .models import CustomUser, Product, Category, Cart
 
 class CustomAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name','last_name')
+    list_display = ['username', 'email', 'first_name','last_name']
 
 admin.site.register(CustomUser, CustomAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'featured')
+    list_display = ['name', 'price', 'featured']
 
 admin.site.register(Product, ProductAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
+    list_display = ['name', 'slug']
 
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register([Cart])
