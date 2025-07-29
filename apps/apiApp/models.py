27f3c909f23a01722233efd88febbe66ecddc605
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-07-29 23:53:10 +0100

    feat: create the model for review

--- apps/apiApp/models.py
+++ apps/apiApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
 
@@ -66,3 +67,27 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f'{self.quantity} x {self.product.name} in cart {self.cart.cart_code}'
+
+class Review(models.Model):
+
+    RATINGS_CHOICES = [
+        (1, 'Poor'),
+        (2, 'Fair'),
+        (3, 'Good'),
+        (4, 'Very Good'),
+        (5, 'Excellent'),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveIntegerField(choices=RATINGS_CHOICES)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s review of {self.product.name}"
+    
+    class Meta:
+        unique_together = ['product', 'user']
+        ordering = ['-created_at']
