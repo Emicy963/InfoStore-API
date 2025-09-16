1f478af0d4cd205262d773075b053035c7506896
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-16 09:46:45 +0100

    feat(views): Create the API Views for logout

--- apps/apiApp/views.py
+++ apps/apiApp/views.py
@@ -3,8 +3,9 @@ from django.db.models import Q
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product, Category, Cart, CartItem, Review, Wishlist
 from .serializers import (
     ProducListSerializer,
@@ -54,6 +55,25 @@ def register(request):
         {"message": "User created successfully."},
         status=status.HTTP_201_CREATED
     )
+
+
+@api_view["POST"]
+@permission_classes[IsAuthenticated]
+def logout(request):
+    try:
+        refresh_token = request.data.get("refresh")
+        if refresh_token:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        return Response(
+            {"message": "Succesfully logged out"},
+            status=status.HTTP_200_OK
+        )
+    except Exception as e:
+        return Response(
+            {"error": str(e)},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 @api_view(["GET"])
