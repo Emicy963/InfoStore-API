1c01300b06cbf5bfe947b1fab578d5fb53f0cba1
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-27 00:32:50 +0100

    feat(urls): Updated the endpoints

--- apps/apiApp/urls.py
+++ apps/apiApp/urls.py
@@ -8,23 +8,28 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/register/", views.register, name="register"),
     path("api/logout/", views.logout, name="logout"),
-    path("profile/", views.get_user_profile, name="user_profile"),
+    path("api/profile/", views.get_user_profile, name="user_profile"),
+    # Cart
+    path("cart/<str:cart_code>/", views.get_cart, name="get_cart"),
+    path("api/cart/create/", views.create_user_cart, name="create_user_cart"),
+    path("api/cart/user/", views.get_user_cart, name="get_user_cart"),
+    path("api/cart/merge/", views.merge_carts, name="merge_carts"),
+    path("add_to_cart/", views.add_to_cart, name="add_to_cart"),
+    path("update_cartitem_quantity/", views.update_cartitem_quantity, name="update_cartitem_quantity"),
+    path("delete_cartitem/<int:pk>/", views.delete_cartitem, name="delete_cartitem"),
     # Product and Category
     path("product_list", views.product_list, name="product_list"),
     path("products/<slug:slug>", views.product_detail, name="product_detail"),
     path("category_list", views.category_list, name="category_list"),
-    path("categories/<slug:slug>", views.category_detail, name="category_deatil"),
-    path("add_to_cart/", views.add_to_cart, name="add_to_cart"),
-    path("cart/<str:cart_code>/", views.get_cart, name="get_cart"),
-    path(
-        "update_cartitem_quantity/",
-        views.update_cartitem_quantity,
-        name="update_cartitem_quantity",
-    ),
+    path("categories/<slug:slug>", views.category_detail, name="category_detail"),
+    # Wishlist
+    path("api/wishlist/", views.get_user_wishlist, name="get_user_wishlist"),
+    path("api/wishlist/<int:pk>/", views.delete_wishlist_item, name="delete_wishlist_item"),
+    path("add_to_wishlist/", views.add_to_wishlist, name="add_to_wishlist"),
+    # Reviews
     path("add_review/", views.add_review, name="add_review"),
     path("update_review/<int:pk>/", views.update_review, name="update_review"),
     path("delete_review/<int:pk>/", views.delete_review, name="delete_review"),
-    path("delete_cartitem/<int:pk>/", views.delete_cartitem, name="delete_cartitem"),
-    path("add_to_wishlist/", views.add_to_wishlist, name="add_to_wishlist"),
+    # Search
     path("search/", views.product_search, name="search"),
 ]
