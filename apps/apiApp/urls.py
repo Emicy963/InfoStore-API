f21e1072a8031c41db780b1aa5f7870c0b14b846
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-27 09:47:29 +0100

    fix(urls): Add on EndPoints the url for found the cart with code

--- apps/apiApp/urls.py
+++ apps/apiApp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("api/register/", views.register, name="register"),
     path("api/logout/", views.logout, name="logout"),
     path("api/profile/", views.get_user_profile, name="user_profile"),
+    
     # Cart
     path("cart/<str:cart_code>/", views.get_cart, name="get_cart"),
     path("api/cart/create/", views.create_user_cart, name="create_user_cart"),
@@ -17,19 +18,23 @@ urlpatterns = [
     path("add_to_cart/", views.add_to_cart, name="add_to_cart"),
     path("update_cartitem_quantity/", views.update_cartitem_quantity, name="update_cartitem_quantity"),
     path("delete_cartitem/<int:pk>/", views.delete_cartitem, name="delete_cartitem"),
+    
     # Product and Category
     path("product_list", views.product_list, name="product_list"),
     path("products/<slug:slug>", views.product_detail, name="product_detail"),
     path("category_list", views.category_list, name="category_list"),
-    path("categories/<slug:slug>", views.category_detail, name="category_detail"),
+    path("categories/<slug:slug>", views.category_detail, name="category_deatil"),
+    
     # Wishlist
     path("api/wishlist/", views.get_user_wishlist, name="get_user_wishlist"),
     path("api/wishlist/<int:pk>/", views.delete_wishlist_item, name="delete_wishlist_item"),
     path("add_to_wishlist/", views.add_to_wishlist, name="add_to_wishlist"),
+    
     # Reviews
     path("add_review/", views.add_review, name="add_review"),
     path("update_review/<int:pk>/", views.update_review, name="update_review"),
     path("delete_review/<int:pk>/", views.delete_review, name="delete_review"),
+    
     # Search
     path("search/", views.product_search, name="search"),
 ]
