b89a8e8d4a6245a5693120d6c6a9d876f0c5a127
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-15 03:06:15 +0100

    fix(serializer): Making the resgister validation on serializer and solve some sintaxe problem.

--- apps/apiApp/serializers.py
+++ apps/apiApp/serializers.py
@@ -6,19 +6,71 @@ from .models import Product, Category, CartItem, Cart, Review, Wishlist
 
 User = get_user_model()
 
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    confirm_password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "confirm_password", "first_name", "last_name", "phone_number", "bi"]
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        username = attrs.get("username")
+
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("Este email já está em uso.")
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError("Este nome de usuário já está em uso.")
+        if attrs["password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError("As palavras-passes não são iguais.")
+        return attrs
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(password=password, **validated_data)
+        return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        # The field 'username' on request and be username or email
+        identifier = attrs.get("username")
+        password = attrs.get("password")
+
+        # Try email first
+        if identifier and password:
+            try:
+                user_obj = User.objects.get(email=identifier)
+                attrs["username"] = user_obj.username # Chance the real username for father validation
+            except User.DoesNotExist:
+                # If don't founde, use the username
+                pass
+
+        data = super().validate(attrs)
+        data["user"] = {
+            "id": self.user.id,
+            "email": self.user.email,
+            "username": self.user.username,
+            "name": self.user.get_full_name() or self.user.username,
+        }
+        return data
+    
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name", "avatar_url"]
+        fields = ["id", "username", "first_name", "last_name", "email", "phone_number", "bi", "avatar_url"]
 
 
-class ProducListSerializer(serializers.ModelSerializer):
+class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "name", "slug", "image", "price"]
+        fields = ["id", "name", "slug", "description", "image", "price", "average_rating", "total_reviews"]
 
 
-class ProducDetailSerializer(serializers.ModelSerializer):
+class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "name", "slug", "description", "image", "price"]
@@ -31,7 +83,7 @@ class CategoryListSerialiizer(serializers.ModelSerializer):
 
 
 class CategoryDetailSerialiizer(serializers.ModelSerializer):
-    products = ProducListSerializer(many=True, read_only=True)
+    products = ProductListSerializer(many=True, read_only=True)
 
     class Meta:
         model = Category
@@ -39,7 +91,7 @@ class CategoryDetailSerialiizer(serializers.ModelSerializer):
 
 
 class CartItemSerializer(serializers.ModelSerializer):
-    product = ProducListSerializer(read_only=True)
+    product = ProductListSerializer(read_only=True)
     sub_total = serializers.SerializerMethodField()
 
     class Meta:
@@ -88,38 +140,8 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 class WishListSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
-    product = ProducListSerializer(read_only=True)
+    product = ProductListSerializer(read_only=True)
 
     class Meta:
         model = Wishlist
         fields = ["id", "user", "product", "created_at"]
-
-
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token["email"] = user.email
-        token["name"] = user.get_full_name() or user.username
-        return token
-    
-    def validate(self, attrs):
-        identifier = attrs.get("username")
-        password = attrs.get("password")
-
-        if identifier and password:
-            try:
-                user_obj = User.objects.get(email=identifier)
-                # If found by email, set username to email
-                attrs["username"] = user_obj.username
-            except User.DoesNotExist:
-                # If not found by email, proceed with username
-                pass
-
-        data = super().validate(attrs)
-        data["user"] = {
-            "id": self.user.id,
-            "email": self.user.email,
-            "name": self.user.get_full_name() or self.user.username,
-        }
-        return data
