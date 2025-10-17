07b213fab6fc04270161ed0dd77b47db294138ae
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-17 02:00:00 +0100

    feat(ursl): Create the endpoints for orders

--- apps/apiApp/urls.py
+++ apps/apiApp/urls.py
@@ -38,6 +38,10 @@ urlpatterns = [
     path("reviews/add/", views.add_review, name="add_review"),
     path("reviews/<int:pk>/update/", views.update_review, name="update_review"),
     path("reviews/<int:pk>/delete/", views.delete_review, name="delete_review"),
+    # Orders
+    path("orders/create/", views.create_order, name="create_order"),
+    path("api/orders/", views.get_user_orders, name="get_user_orders"),
+    path("api/orders/<int:pk>/", views.get_order_detail, name="get_order_detail"),
     # Search
     path("search/", views.product_search, name="search"),
 ]
