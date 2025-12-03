e0dd7a55f5cfbd6072fdf0c5cc66cbd3354dada0
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 00:01:26 +0100

    feat: Config the Pagination on views.

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+class ProductPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 100
