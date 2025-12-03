a18a20609dd8a36072d6394de51caecfa86041c9
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-03 23:50:50 +0100

    feat(models): Create Product and Category models.

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to="category_img", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique_slug = self.slug
+            counter = 1
+            while Category.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+            self.slug = unique_slug
+        super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to="product_img", blank=True, null=True)
+    featured = models.BooleanField(default=True)
+    cateagory = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="products",
+        blank=True,
+        null=True,
+    )
+    average_rating = models.FloatField(default=0.0)
+    total_reviews = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["-id"]
+        # ordering = ["-average_rating"]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique_slug = self.slug
+            counter = 1
+            if Product.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+            self.slug = unique_slug
+        super().save(*args, **kwargs)
