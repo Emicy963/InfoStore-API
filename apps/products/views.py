4672afa2ad8153bf9db9f283e2cb28e7f6dd4a9c
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 00:13:12 +0100

    feat: Create the view for list categories.

--- apps/products/views.py
+++ apps/products/views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductDetailSerializer, ProductListSerializer
+from .models import Category, Product
+from .serializers import CategoryListSerialiizer, ProductDetailSerializer, ProductListSerializer
 
 
 class ProductPagination(PageNumberPagination):
@@ -33,4 +33,17 @@ def product_detail(request, slug):
     except Product.DoesNotExist:
         return Response(
             {"error": "Produto não encontrado."}, status=status.HTTP_404_NOT_FOUND
+        )
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def category_list(request):
+    try:
+        categories = Category.objects.all()
+        serializer = CategoryListSerialiizer(categories, many=True)
+        return Response(serializer.data)
+    except Category.DoesNotExist:
+        Response(
+            {"error": "Categorias não encontrado."}, status=status.HTTP_404_NOT_FOUND
         )
