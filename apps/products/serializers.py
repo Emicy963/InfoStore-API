b2c133a9249d8259580032c636ca7c95b3d74794
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-03 23:56:14 +0100

    feat: Create the serializer for list category.

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -21,3 +21,8 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "name", "slug", "description", "image", "price"]
+
+class CategoryListSerialiizer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "image", "slug"]
