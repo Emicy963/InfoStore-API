d912d7c7c6a013560ee4c0ecd9cf00510274f8a4
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 00:21:56 +0100

    feat: Create the endpoints.

---
+++ apps/products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # Product and Category
+    path("", views.product_list, name="product_list"),
+    path("<slug:slug>/", views.product_detail, name="product_detail"),
+    path("categories/", views.category_list, name="category_list"),
+    path("categories/<slug:slug>/", views.category_detail, name="category_detail"),
+]
