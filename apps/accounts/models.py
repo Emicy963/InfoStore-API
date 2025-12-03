ec9d065baba667e3ec5acf24f268e2ca24e3acae
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-03 22:33:38 +0100

    feat(models): Pass the models from apiAPP to accounts app.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    bi = models.CharField(
+        "BI/Passaporte", max_length=30, blank=True, null=True, unique=True
+    )
+    avatar_url = models.URLField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, default="Angola")
+
+    def __str__(self):
+        return f"{self.username} ({self.email})"
--- apps/apiApp/models.py
+++ apps/apiApp/models.py
@@ -5,22 +5,6 @@ from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 
 
-class CustomUser(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    bi = models.CharField(
-        "BI/Passaporte", max_length=30, blank=True, null=True, unique=True
-    )
-    avatar_url = models.URLField(blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    city = models.CharField(max_length=100, blank=True, null=True)
-    country = models.CharField(max_length=100, default="Angola")
-
-    def __str__(self):
-        return f"{self.username} ({self.email})"
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, blank=True)
