04cf8b739ce42e86f1e06f569d17f03ee8ee0d4a
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 00:47:48 +0100

    feat: Create the Cart Item model.

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from apps.products.models import Product
 
 
 class Cart(models.Model):
@@ -25,3 +26,12 @@ class Cart(models.Model):
 
     def __str__(self):
         return self.cart_code
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cartitems")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="item")
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in cart {self.cart.cart_code}"
