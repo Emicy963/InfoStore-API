8ffa42f69692df2436de4735c014b2a90025d0b3
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 02:10:11 +0100

    feat: Create the Order Item model.

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from apps.products.models import Product
 
 
 class Order(models.Model):
@@ -45,3 +46,13 @@ class Order(models.Model):
                 random.choices(string.ascii_uppercase + string.digits, k=10)
             )
         super().save(*args, **kwargs)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in order {self.order.order_code}"
