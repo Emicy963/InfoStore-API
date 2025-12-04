b4f61a1d95f867e4b80aa90e2f3bad2ace3e47ca
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 02:00:09 +0100

    feat: Working in wishlist views: - Add product to wishlist; - Get the wishlist; - Delete the wishlist.

--- apps/wishlist/views.py
+++ apps/wishlist/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Wishlist
+from .serializers import WishListSerializer
+from apps.products.models import Product
 
-# Create your views here.
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def add_to_wishlist(request):
+    product_id = request.data.get("product_id")
+
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return Response(
+            {"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND
+        )
+
+    user = request.user
+
+    wishlist_item, created = Wishlist.objects.get_or_create(user=user, product=product)
+
+    if not created:
+        wishlist_item.delete()
+        return Response(
+            {"message": "Product removed from wishlist"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
+
+    serializer = WishListSerializer(wishlist_item)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_user_wishlist(request):
+    wishlist_itmes = Wishlist.objects.filter(user=request.user)
+    serializer = WishListSerializer(wishlist_itmes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+@permission_classes([IsAuthenticated])
+def delete_wishlist_item(request, pk):
+    try:
+        wishlist_item = Wishlist.objects.get(pk=pk, user=request.user)
+        wishlist_item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Wishlist.DoesNotExist:
+        return Response(
+            {"error": "Wishlist item not found"}, status=status.HTTP_404_NOT_FOUND
+        )
