db1814204471c95a0951477a9b924988fa4d2043
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 01:06:53 +0100

    feat: Working in cart views: - Update the cart quantity; - Delete the cartitem; - Merge the carts when anonymos user get auth.

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,9 +1,9 @@
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from .models import Cart, CartItem
-from .serializers import CartSerializer
+from .serializers import CartItemSerializer, CartSerializer
 from apps.products.models import Product
 
 
@@ -111,5 +111,114 @@ def add_to_cart(request):
         return Response(
             {"error": "Produto não encontrado."}, status=status.HTTP_404_NOT_FOUND
         )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def update_cartitem_quantity(request):
+    cartitem_id = request.data.get("item_id")
+    quantity = request.data.get("quantity")
+
+    try:
+        quantity = int(quantity)
+        if quantity <= 0:
+            return Response(
+                {"error": "A quantidade deve ser maior que zero."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        cartitem = CartItem.objects.get(id=cartitem_id)
+
+        if cartitem.cart.user and cartitem.cart.user != request.user:
+            return Response(
+                {"error": "Você não tem permissão para modificar este item."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        cartitem.quantity = quantity
+        cartitem.save()
+
+        serializer = CartItemSerializer(cartitem)
+        return Response(
+            {
+                "data": serializer.data,
+                "message": "Item do carrinho atualizado com sucesso!",
+            }
+        )
+    except CartItem.DoesNotExist:
+        return Response(
+            {"error": "Item do carrinho não encontrado."},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    except ValueError:
+        return Response(
+            {"error": "Quantidade inválida."}, status=status.HTTP_400_BAD_REQUEST
+        )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+@permission_classes([IsAuthenticated])
+def delete_cartitem(request, pk):
+    try:
+        cartitem = CartItem.objects.get(id=pk)
+
+        if cartitem.cart.user and cartitem.cart.user != request.user:
+            return Response(
+                {"error": "Você não tem permissão para excluir este item"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        cartitem.delete()
+        return Response(
+            {"message": "Item do carrinho excluído com sucesso"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
+    except CartItem.DoesNotExist:
+        return Response(
+            {"error": "Item do carrinho não encontrado"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def merge_carts(request):
+    try:
+        # Get the user cart
+        user_cart, created = Cart.objects.get_or_create(user=request.user)
+        if created:
+            # Generate an unique code for cart
+            import random
+            import string
+
+            user_cart.cart_code = "".join(
+                random.choices(string.ascii_letters + string.digits, k=11)
+            )
+            user_cart.save()
+
+        # Get the temporary cart (if exist)
+        temp_cart_code = request.data.get("temp_cart_code")
+        if temp_cart_code:
+            try:
+                temp_cart = Cart.objects.get(cart_code=temp_cart_code)
+                # Move items from temporary cart to user cart
+                for item in temp_cart.cartitems.all():
+                    CartItem.objects.update_or_create(
+                        cart=user_cart,
+                        product=item.product,
+                        defaults={"quantity": item.quantity},
+                    )
+                temp_cart.delete()
+            except Cart.DoesNotExist:
+                pass
+
+        serializer = CartSerializer(user_cart)
+        return Response(serializer.data)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
