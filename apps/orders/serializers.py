6aaa32512d9342b5d4288f067b24e97b13aa572a
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 02:13:35 +0100

    feat: Create the serializer for order item.

--- apps/orders/serializers.py
+++ apps/orders/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
+from .models import OrderItem
+from apps.products.serializers import ProductListSerializer
 
 
 class CreateOrderSerializer(serializers.Serializer):
     shipping_address = serializers.JSONField()
     payment_method = serializers.CharField()
     notes = serializers.CharField(required=False, allow_blank=True)
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ["id", "product", "quantity", "price"]
