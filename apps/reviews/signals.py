6962218d9a0aa1916ce587a89845c97ba2eec85e
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-04 01:43:23 +0100

    feat: Create and config the signal.

---
+++ apps/reviews/signals.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.db.models import Avg
+from .models import Review
+
+
+@receiver(post_save, sender=Review)
+def update_product_rating_on_save(sender, instance, **kwargs):
+    product = instance.product
+    reviews = product.reviews.all()
+
+    # Update on Product
+    product.total_reviews = reviews.count()
+    product.average_rating = reviews.aggregate(Avg("rating"))["rating__avg"] or 0.0
+    product.save()
+
+
+@receiver(post_delete, sender=Review)
+def update_product_rating_on_delete(sender, instance, **kwargs):
+    product = instance.product
+    reviews = product.reviews.all()
+
+    # Update on Product
+    product.total_reviews = reviews.count()
+    product.average_rating = reviews.aggregate(Avg("rating"))["rating__avg"] or 0.0
+    product.save()
