1883d802660327e987c73ceb75a990e72d618756
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-05 02:30:09 +0100

    tests: Start the test with the Cart Model: - Test create the new cart.

--- apps/cart/tests.py
+++ apps/cart/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Cart
 
-# Create your tests here.
+
+User = get_user_model()
+
+class CartModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser123",
+            email="test@example.com",
+        )
+
+        self.cart_data = {
+            "user":self.user,
+            "cart_code":"test123",
+        }
+    
+    def test_create_cart(self):
+        cart = Cart.objects.create(**self.cart_data)
+        self.assertEqual(cart.user.username, "testuser123")
+        self.assertEqual(cart.user.email, "test@example.com")
+        self.assertEqual(cart.cart_code, "test123")
