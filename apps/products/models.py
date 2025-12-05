1d39df3e6f66eb7443fe4811fec2ff75d563569b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-05 01:50:18 +0100

    fix: Fix the Product Model and change the field from "cateogory" to "category".

--- apps/products/models.py
+++ apps/products/models.py
@@ -24,12 +24,12 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     slug = models.SlugField(unique=True, blank=True)
     image = models.ImageField(upload_to="product_img", blank=True, null=True)
     featured = models.BooleanField(default=True)
-    cateagory = models.ForeignKey(
+    category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         related_name="products",
