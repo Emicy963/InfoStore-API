386fac7ad576849fcb0edd90f05a07ac3412084b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-05 02:08:26 +0100

    tests: Create the tests for Category model: - Test the create the category.

--- apps/products/tests.py
+++ apps/products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Category, Product
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        self.category_data = {
+            "name": "CategoryTest",
+            # "slug": "categorytest",
+        }
+    
+    def test_create_category(self):
+        category = Category.objects.create(**self.category_data)
+        self.assertEqual(category.name, "CategoryTest")
+        self.assertEqual(category.slug, "categorytest")
