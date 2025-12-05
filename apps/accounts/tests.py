ab144f1be9be7d7ac44a65e609469b2051ab0735
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-05 01:31:37 +0100

    tests: Create the tests for user model: - Teste the create the normal user.

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+User = get_user_model()
+
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.data_user = {
+            "username": "testuser123",
+            "email": "test@example.com",
+            "phone_number": "9123456789",
+            "bi": "1234567HO123",
+            "address": "Luanda, Angola",
+            "city": "Luanda",
+            "country": "Angola",
+        }
+    
+    def test_create_user(self):
+        user = User.objects.create_user(**self.data_user)
+        self.assertEqual(user.username, "testuser123")
+        self.assertEqual(user.email, "test@example.com")
+        self.assertEqual(user.phone_number, "9123456789")
+        self.assertEqual(user.bi, "1234567HO123")
+        self.assertEqual(user.address, "Luanda, Angola")
+        self.assertEqual(user.city, "Luanda")
+        self.assertEqual(user.country, "Angola")
